49db788aea69fcefb605364a78c3cbffe2fb5cf7
Alec Schneider <alecschneider@me.com>
2018-05-15 16:27:32 -0400

    moved some files

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pprint
-from coinmarketcap import Market
 from collections import OrderedDict
 from binance.client import Client
 from index import calc_allocation, rebalance
@@ -55,39 +54,6 @@ def get_lot_size(filters):
             return f
 
 
-def get_holdings(client):
-    return {
-            coin["asset"]: float(coin["free"]) + float(coin["locked"])
-            for coin in client.get_account()['balances']
-            if float(coin["free"]) + float(coin["locked"]) > 0
-    }
-
-
-def get_prices(client, currency):
-    prices = {currency: 1}
-    for coin in client.get_all_tickers():
-        if currency not in coin["symbol"]:
-            continue
-        symbol = coin["symbol"].replace(currency, "")
-        prices[symbol] = float(coin["price"])
-    return prices
-
-
-def get_market_caps(top=100):
-    coinmarketcap = Market()
-    caps = [
-            {"symbol": coin["symbol"], "value": coin["market_cap_usd"]}
-            for coin in coinmarketcap.ticker(start=0, limit=top)
-            ]
-    return caps
-
-
-def calc_portfolio_value(prices, holdings):
-    return sum([
-        amount * prices[symbol]
-        if symbol in prices else amount
-        for symbol, amount in holdings.items()
-    ])
 
 
 def main():
--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,41 @@
 from binance.client import Client
+from coinmarketcap import Market
 from keys import api_key, api_secret
-from main import get_holdings, get_prices, calc_portfolio_value
+
+
+def get_holdings(client):
+    return {
+            coin["asset"]: float(coin["free"]) + float(coin["locked"])
+            for coin in client.get_account()['balances']
+            if float(coin["free"]) + float(coin["locked"]) > 0
+    }
+
+
+def get_prices(client, currency):
+    prices = {currency: 1}
+    for coin in client.get_all_tickers():
+        if currency not in coin["symbol"]:
+            continue
+        symbol = coin["symbol"].replace(currency, "")
+        prices[symbol] = float(coin["price"])
+    return prices
+
+
+def get_market_caps(top=100):
+    coinmarketcap = Market()
+    caps = [
+            {"symbol": coin["symbol"], "value": coin["market_cap_usd"]}
+            for coin in coinmarketcap.ticker(start=0, limit=top)
+            ]
+    return caps
+
+
+def calc_portfolio_value(prices, holdings):
+    return sum([
+        amount * prices[symbol]
+        if symbol in prices else amount
+        for symbol, amount in holdings.items()
+    ])
 
 
 def calc_allocation(value, prices, holdings):
@@ -9,9 +44,11 @@ def calc_allocation(value, prices, holdings):
             for symbol, amount in holdings.items()
             ]
 
+
 def print_portfolio(allocation):
     for s, a in sorted(allocation, key=lambda tup: tup[1], reverse=True):
         print(s, ":", a, "%")
+
 
 currency = "BTC"
 client = Client(api_key, api_secret)
@@ -22,4 +59,3 @@ allocation = calc_allocation(value, prices, holdings)
 
 print(value * prices["USDT"], "$")
 print_portfolio(allocation)
-
